8c4de3ebe595985abf2ddda731ae3c04a881be80
leo27945875 <ezioatiar@gmail.com>
2024-11-04 23:20:39 +0800

    Fix README & add unittest

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 ## A simple implementation of the UNet of SD v1.5.
 
+It can directly load the SD model config and weights in the `diffusers`.
+
+```python
+from diffusers import StableDiffusionPipeline
+from simple_sd_unet.unet import Cond_UNet2DModel
+
+pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+unet = Cond_UNet2DModel(pipe.unet.config)
+unet.load_state_dict(pipe.unet.state_dict())
+```
+
 ![](figures/unet.png)
--- test_arch.py
+++ test_arch.py
@@ -1,18 +1,25 @@
+import unittest
+
 import torch
 from torchinfo import summary
+
+from diffusers import StableDiffusionPipeline
 
 from simple_sd_unet.unet import Cond_UNet2DModel
 from simple_sd_unet.utils import load_json
 
 
-def test_unet_arch():
-    cfg = load_json("sd_unet_v15/unet.json")
-    wgt = torch.load("sd_unet_v15/unet.pth", map_location="cpu")
-    unet = Cond_UNet2DModel(cfg)
-    unet.load_state_dict(wgt)
-    summary(unet)
+class TestUNet(unittest.TestCase):
+    def test_unet_arch(self):
+        cfg = load_json("sd_unet_v15/unet.json")
+        wgt = torch.load("sd_unet_v15/unet.pth", map_location="cpu")
+        unet = Cond_UNet2DModel(cfg)
+        unet.load_state_dict(wgt)
+        summary(unet)
 
+    def test_load_pipeline(self):
+        pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+        unet = Cond_UNet2DModel(pipe.unet.config)
+        unet.load_state_dict(pipe.unet.state_dict())
+        summary(unet)
 
-if __name__ == "__main__":
-
-    test_unet_arch()
